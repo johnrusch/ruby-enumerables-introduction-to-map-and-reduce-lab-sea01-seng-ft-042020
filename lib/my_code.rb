704b09f4eee032726f0f6d81ee2958e79c7dbb35
John Rusch <johnrusch7@gmail.com>
2020-03-27 00:35:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -33,8 +33,9 @@ def map_to_square(source_array)
   end
   new_array
 end
-binding.pry
+
 def reduce_to_total(source_array, starting_point)
+  starting_point
   counter = 0 
   total = 0 
   binding.pry
