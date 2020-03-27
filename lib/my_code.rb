555292e0c6a54f7009f3360817b9b8340bfcf329
John Rusch <johnrusch7@gmail.com>
2020-03-27 00:33:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -38,9 +38,9 @@ def reduce_to_total(source_array, starting_point)
   counter = 0 
   total = 0 
   binding.pry
-  while counter < source_array.length do
-    total += source_array[counter]
-    counter += 1 
-  end
+  # while counter < source_array.length do
+  #   total += source_array[counter]
+  #   counter += 1 
+  # end
   total
 end
