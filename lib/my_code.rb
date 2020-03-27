ad4235ea02140567ee95c4a604293b400b6cede8
John Rusch <johnrusch7@gmail.com>
2020-03-27 00:30:03 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -14,11 +14,11 @@ def map_to_no_change(source_array)
   source_array
 end
 
-def map_to_negativize(source_array)
+def map_to_double(source_array)
   counter = 0
   new_array = []
   while counter < source_array.length do
-    new_array << source_array[counter] * -1
+    new_array << source_array[counter] * 2
     counter += 1 
   end
   new_array
